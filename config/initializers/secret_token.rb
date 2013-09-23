f4f6a9cdbf77ef1fc6d9331b05a5cae865c9f2bc
delaneyphx <delaneyphx@gmail.com>
2013-09-23 08:51:21 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yawlr::Application.config.secret_key_base = '2882ae36495ce61b1944b9dd226ae3ad73ec0593f4fab12e2292416151b10074c996132f870a33af7e2e23f5233f042f1e650a1a93fd975f68a9bb651ab88ab2'
